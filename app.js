e995f9ea100200d6ebb12f3b50f9ffdcbd4d7a73
Boris <boris.valli@epitech.eu>
2016-11-27 14:51:47 +0100

    changement operation

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-var extra = 0;
-
 function updateContactList() {
 	cozysdk.defineRequest('BankOperation', 'all', 'function(doc) {emit(doc.date);}', function(err, res) {
 		if (err != null) {
@@ -58,10 +56,97 @@ function soldeMoisPrecedent() {
 					//	operations.doc.amount = operation.doc.amount;
 					//});
 					soldeopMoiPrecedent(operations);
+		
+
 				}
 			});
 		}
 	});
+}
+
+
+var extra = 0;
+
+function soldeTotal(callback) {
+cozysdk.defineRequest('BankOperation', 'all', 'function(doc) {emit(doc.date);}', function(err, res) {
+		if (err != null) {
+			return alert(err);
+		} else {
+//			cozysdk.run('BankOperation', 'all', {"include_docs":true,"startkey":"2016-11","endkey":"2016-12"}, function(err, operations) {
+			cozysdk.run('BankOperation', 'all', {"include_docs":true,}, function(err, operations) {
+				if (err != null) {
+					return alert(err);
+				} else {
+
+					operations = JSON.parse("" + operations);
+					//operations.forEach(function(operation) {
+					//	operations.doc.amount = operation.doc.amount;
+					//});
+					extra = soldeopb(operations);
+					callback();
+				}
+			});
+		}
+	});
+}
+
+function soldeMoisPrec(callback) {
+
+cozysdk.defineRequest('BankOperation', 'all', 'function(doc) {emit(doc.date);}', function(err, res) {
+		if (err != null) {
+			return alert(err);
+		} else {
+//			cozysdk.run('BankOperation', 'all', {"include_docs":true,"startkey":"2016-11","endkey":"2016-12"}, function(err, operations) {
+			cozysdk.run('BankOperation', 'all', {"include_docs":true,"endkey":"2016-11"}, function(err, operations) {
+				if (err != null) {
+					return alert(err);
+				} else {
+
+					operations = JSON.parse("" + operations);	
+					//operations.forEach(function(operation) {
+					//	operations.doc.amount = operation.doc.amount;
+					//});
+					extra -= soldeopMoiPrecedentb(operations);
+					callback();
+		
+				}
+			});
+		}
+	});
+}
+
+function calculateExtra() {
+
+soldeTotal(function() {	
+	soldeMoisPrec(function() {
+
+		var HTML= '';
+		var template = 'Extra = '+ extra;
+		HTML = HTML + template;
+		document.querySelector('.extra').innerHTML = HTML;
+
+	})
+});
+}
+
+function soldeopb(operations)
+{
+	var solde = 0;
+	for (i = 0; i < operations.length; i++)
+	{
+		solde += operations[i].doc.amount;
+	}
+return (solde);
+}
+
+function soldeopMoiPrecedentb(operations)
+{
+	var solde = 0;
+	for (i = 0; i < operations.length; i++)
+	{
+		solde += operations[i].doc.amount;
+	}
+return (solde);
 }
 
 function soldeop(operations)
@@ -73,9 +158,9 @@ function soldeop(operations)
 		solde += operations[i].doc.amount;
 	}
 	var aff = '<h1> Solde : '+ solde + '<h1>';
-	extra += solde;
 	HTML = HTML + aff;
 	document.querySelector('.solde').innerHTML = HTML;
+return (solde);
 }
 
 function soldeopMoiPrecedent(operations)
@@ -86,10 +171,10 @@ function soldeopMoiPrecedent(operations)
 	{
 		solde += operations[i].doc.amount;
 	}
-	extra -= solde;
 	var aff = '<h1>Au moi précédent tu avais : '+ solde + '<h1>';
 	HTML = HTML + aff;
 	document.querySelector('.soldeMoiPrecedent').innerHTML = HTML;
+return (solde);
 }
 
 function render(operations) {
@@ -117,10 +202,10 @@ function renderb(contacts) {
 	document.querySelector('.contact-list').innerHTML = HTML;
 }
 
-var HTML= '';
-var template = 'Extra = '+extra;
-HTML = HTML + template;
-document.querySelector('.extra').innerHTML = HTML;
+
 document.addEventListener("DOMContentLoaded", updateContactList);
 document.addEventListener("DOMContentLoaded", solde);
 document.addEventListener("DOMContentLoaded", soldeMoisPrecedent);
+document.addEventListener("DOMContentLoaded", calculateExtra);
+
+
